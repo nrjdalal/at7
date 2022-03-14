d676be5bbaddf2d83ecdfc42b15e58421e0c8776
Neeraj Dalal <admin@nrjdalal.com>
2022-03-14 21:03:18 +0530

    Mon Mar 14 21:03:18 IST 2022

---
+++ data.js
@@ -0,0 +1,44 @@
+export const store = {
+  logo: '/images/logo.png',
+  description: `Domino’s came from humble beginnings in 1960, with just one store. Now, as the largest pizza company in the
+  world, Domino’s proudly continues its legacy of delivering great-tasting pizza to customers’ doors.`,
+}
+
+export const items = [
+  {
+    xid: 'Cd7TaH',
+    category: 'Pizza',
+    name: 'Margherita',
+    description: 'Classic delight with 100% real mozzarella cheese',
+    price: '219',
+    src: '/images/Margherita.jpg',
+    nonveg: false,
+  },
+  {
+    xid: 'AiC3Kj',
+    category: 'Pizza',
+    name: 'Farmhouse',
+    description: 'Delightful combination of onion, capsicum, tomato & grilled mushroom',
+    price: '419',
+    src: '/images/Farmhouse.jpg',
+    nonveg: false,
+  },
+  {
+    xid: 'VwJ3Ej',
+    category: 'Pizza',
+    name: 'Elite One',
+    description: '',
+    price: '799',
+    src: '',
+    nonveg: true,
+  },
+  {
+    xid: '0hAK0z',
+    category: 'Pizza',
+    name: 'Supreme One',
+    description: '',
+    price: '999',
+    src: '',
+    nonveg: true,
+  },
+]
--- pages/order.tsx
+++ pages/order.tsx
@@ -1,42 +1,9 @@
 /* eslint-disable @next/next/no-img-element */
 
-const items = [
-  {
-    xid: 'Cd7TaH',
-    name: 'Margherita',
-    description: 'Classic delight with 100% real mozzarella cheese',
-    price: '219',
-    src: '/images/Margherita.jpg',
-    nonveg: false,
-  },
-  {
-    xid: 'AiC3Kj',
-    name: 'Farmhouse',
-    description: 'Delightful combination of onion, capsicum, tomato & grilled mushroom',
-    price: '419',
-    src: '/images/Farmhouse.jpg',
-    nonveg: false,
-  },
-  {
-    xid: 'VwJ3Ej',
-    name: 'Elite One',
-    description: '',
-    price: '799',
-    src: '',
-    nonveg: true,
-  },
-  {
-    xid: '0hAK0z',
-    name: 'Supreme One',
-    description: '',
-    price: '999',
-    src: '',
-    nonveg: true,
-  },
-]
+import { items, store } from 'data.js'
 
 const index = () => {
-  const Item = ({ description, name, price, src, nonveg }: any) => {
+  const ItemCard = ({ description, name, price, src, nonveg }: any) => {
     return (
       <div className="mx-4 flex items-center gap-4 border-b-[1px] py-4 md:mx-0">
         <div className="flex w-2/3 flex-col gap-2">
@@ -92,21 +59,18 @@ const index = () => {
     <>
       <div className="flex flex-col items-center">
         <div className="flex h-16 w-full items-center justify-center bg-blue-900 md:h-20">
-          <img className="h-1/2" src="/images/logo.png" alt="" />
+          <img className="h-1/2" src={store.logo} alt="" />
         </div>
 
         <div className="mx-4 flex max-w-screen-lg flex-col gap-3 border-b-2 border-dashed py-4 text-sm text-slate-500">
           <p className="text-slate-700">Lajpat Nagar | 9 km</p>
-          <h1>
-            Domino’s came from humble beginnings in 1960, with just one store. Now, as the largest pizza company in the
-            world, Domino’s proudly continues its legacy of delivering great-tasting pizza to customers’ doors.
-          </h1>
+          <h1>{store.description}</h1>
         </div>
 
         <div className="grid w-full max-w-screen-lg md:grid-cols-2 md:gap-x-4 md:px-4 lg:px-0">
           {items.map((item) => {
             return (
-              <Item
+              <ItemCard
                 key={item.xid}
                 name={item.name}
                 description={item.description}
