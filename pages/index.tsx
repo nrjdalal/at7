61e919ecaa3399e1f064cc65d79550a70c598fd5
Neeraj Dalal <admin@nrjdalal.com>
2022-03-14 17:44:21 +0530

    Mon Mar 14 17:44:21 IST 2022

--- pages/index.tsx
+++ pages/index.tsx
@@ -31,12 +31,20 @@ const index = () => {
                 src={src}
                 alt=""
               />
-              <div className="absolute left-1/2 bottom-0 flex w-16 -translate-x-1/2 justify-center rounded-md border-[1.5px] border-green-500 bg-white py-1 font-medium text-green-500">
+              <div
+                className={`absolute left-1/2 bottom-0 flex w-16 -translate-x-1/2 justify-center rounded-md border-[1.5px] bg-white py-1 font-medium ${
+                  nonveg ? 'border-red-500 text-red-500' : 'border-green-500 text-green-500'
+                }`}
+              >
                 Add +
               </div>
             </>
           ) : (
-            <div className="flex w-16 justify-center rounded-md border-[1.5px] border-green-500 bg-white py-1 font-medium text-green-500">
+            <div
+              className={`flex w-16 justify-center rounded-md border-[1.5px] bg-white py-1 font-medium ${
+                nonveg ? 'border-red-500 text-red-500' : 'border-green-500 text-green-500'
+              }`}
+            >
               Add +
             </div>
           )}
