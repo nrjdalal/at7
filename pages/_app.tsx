4d6e6c2eb40c5a1db8006d53e62eae93948d534d
Neeraj Dalal <admin@nrjdalal.com>
2022-03-14 22:48:34 +0530

    Mon Mar 14 22:48:34 IST 2022

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,14 @@
-import '../styles/globals.css'
+import 'styles/globals.css'
+import { RecoilRoot } from 'recoil'
 import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <div className="relative w-screen overflow-x-hidden font-dmSans">
-      <Component {...pageProps} />
-    </div>
+    <RecoilRoot>
+      <div className="relative w-screen overflow-x-hidden font-dmSans">
+        <Component {...pageProps} />
+      </div>
+    </RecoilRoot>
   )
 }
 
